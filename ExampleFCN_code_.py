f66460b4b08be50acd92e36202c31642a96486ed
Tanish Neema <tneema501@gmail.com>
2023-06-13 13:29:45 -0500

    working on second example code

---
+++ ExampleFCN_code_.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+# Define the neural network model
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(NeuralNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+# Define the training loop
+def train(model, train_loader, optimizer, criterion, num_epochs):
+    model.train()
+    for epoch in range(num_epochs):
+        for inputs, targets in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
+
+# Example usage
+input_size = 784  # MNIST image size: 28x28 = 784
+hidden_size = 128
+output_size = 10  # Number of classes in MNIST dataset
+
+# Create an instance of the neural network
+model = NeuralNetwork(input_size, hidden_size, output_size)
+
+# Define the loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# Training loop
+train(model, train_loader, optimizer, criterion, num_epochs=10)
